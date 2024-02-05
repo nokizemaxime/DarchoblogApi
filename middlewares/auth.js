de05f9508f0c8893b878c09d39f92e4ea289145e
Maxime Nokize <93329117+nokizemaxime@users.noreply.github.com>
2024-02-05 18:10:35 +0100

    add modification

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const token = req.headers["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1YzBkM2NkNTVmODUxNGMzMmU2ZmI5ZSIsImlhdCI6MTcwNzE1MjkxNSwiZXhwIjoxNzA3MjM5MzE1fQ.PkEScR1XkRp8JqIr-MG5Br7vG4WyaHTcnSY0T3cwcDQ"];
   if (!token) return res.status(403).json({ message: "Token non fourni." });
 
   jwt.verify(token, config.secret, (err, decoded) => {
